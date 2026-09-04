284413b454ff28dfb446ed147f4b49f6750530b7
beubetMCTX <1515467738@qq.com>
2026-09-05 03:31:04 +0800

    feat: embed Chinese translation resources

--- dpm_manager_zh_CN.ts
+++ dpm_manager_zh_CN.ts
@@ -1,3 +1,437 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="17"/>
+        <source>MainWindow</source>
+        <translation>喷注器管理器</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="31"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="42"/>
+        <source>Species</source>
+        <translation>组分</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="50"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="64"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="74"/>
+        <source>View</source>
+        <translation>视图</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="89"/>
+        <source>Read dpm files</source>
+        <translation>读取 DPM 文件</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="94"/>
+        <source>Read Base Geometry</source>
+        <translation>读取参考几何体</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="99"/>
+        <source>Read Chemkin Files</source>
+        <translation>读取 Chemkin 文件</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="104"/>
+        <source>Unit of Measurement</source>
+        <translation>计量单位</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="109"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="114"/>
+        <source>Species/Colors</source>
+        <translation>组分/颜色</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="119"/>
+        <source>Export Diagnostics</source>
+        <translation>导出诊断信息</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="124"/>
+        <source>Open Config Folder</source>
+        <translation>打开配置文件夹</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="129"/>
+        <source>Open Logs Folder</source>
+        <translation>打开日志文件夹</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="134"/>
+        <source>Save DPM File</source>
+        <translation>保存 DPM 文件</translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="139"/>
+        <source>Open Project Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="144"/>
+        <source>Save Project Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="147"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="152"/>
+        <source>Save Project Session As</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="157"/>
+        <source>Validate Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="162"/>
+        <source>Materials</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="167"/>
+        <source>Objects Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="178"/>
+        <source>Reference Geometry Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="192"/>
+        <source>Reset Window Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="197"/>
+        <source>Undo Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="200"/>
+        <source>Ctrl+Z</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="208"/>
+        <source>Redo Move</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="211"/>
+        <source>Ctrl+Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="219"/>
+        <source>Undo Unit Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="222"/>
+        <source>Ctrl+Shift+Z</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="230"/>
+        <source>Redo Unit Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="233"/>
+        <source>Ctrl+Shift+Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="241"/>
+        <source>Undo Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="244"/>
+        <source>Ctrl+Alt+D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="252"/>
+        <source>Redo Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="255"/>
+        <source>Ctrl+Alt+Shift+D</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="263"/>
+        <source>Undo Reference Transform</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="266"/>
+        <source>Ctrl+Alt+Z</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="274"/>
+        <source>Redo Reference Transform</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/app/mainwindow.ui" line="277"/>
+        <source>Ctrl+Alt+Y</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SpeciesColorDialog</name>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="14"/>
+        <source>Species Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="38"/>
+        <source>Loaded 0 species.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="48"/>
+        <source>Filter species...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="79"/>
+        <source>Species</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="84"/>
+        <source>Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="116"/>
+        <source>Select a species row to edit its color.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="195"/>
+        <source>Save Colors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="202"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_color_dialog.ui" line="209"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SpeciesMaterialDialog</name>
+    <message>
+        <location filename="src/ui/dialogs/species_material_dialog.ui" line="14"/>
+        <source>Materials</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_material_dialog.ui" line="38"/>
+        <source>Materials: 0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_material_dialog.ui" line="48"/>
+        <source>Filter materials...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_material_dialog.ui" line="68"/>
+        <source>Material Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_material_dialog.ui" line="73"/>
+        <source>Density [kg/m^3]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_material_dialog.ui" line="96"/>
+        <source>Add Row</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/species_material_dialog.ui" line="103"/>
+        <source>Remove Row</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>unit_edit_dialog</name>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="31"/>
+        <source>unit_type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="76"/>
+        <source>Injection Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="93"/>
+        <source>Injection Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="110"/>
+        <source>Number of Stream</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="136"/>
+        <source>Particle Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="144"/>
+        <source>Massless</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="151"/>
+        <source>Inert</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="158"/>
+        <source>Droplet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="165"/>
+        <source>Combusting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="194"/>
+        <source>Discrete Phase Domain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="207"/>
+        <source>Evaporating Species</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="223"/>
+        <source>Diameter Distribution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="230"/>
+        <source>Devolatilizing Species</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="237"/>
+        <source>Product Species</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="247"/>
+        <source>Oxidizing Species</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="254"/>
+        <source>Material</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="303"/>
+        <source>Point Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="313"/>
+        <source>Cone Injector Parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="319"/>
+        <source>Cone Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="362"/>
+        <source>GroupBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="375"/>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="381"/>
+        <source>Stagger Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="388"/>
+        <source>Stagger Radius</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="419"/>
+        <source>Physical Models</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="424"/>
+        <source>Turbulent Dispersion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="429"/>
+        <source>Parcel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ui/dialogs/unit_edit_dialog.ui" line="434"/>
+        <source>Wet Combustion</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -20,7 +20,9 @@ int main(int argc, char *argv[])
         const QStringList uiLanguages = QLocale::system().uiLanguages();
         for (const QString &locale : uiLanguages) {
             const QString baseName = "dpm_manager_" + QLocale(locale).name();
-            if (translator.load(":/i18n/" + baseName)) {
+            if (translator.load(":/i18n/" + baseName) ||
+                translator.load(QCoreApplication::applicationDirPath() +
+                                "/i18n/" + baseName)) {
                 a.installTranslator(&translator);
                 break;
             }
